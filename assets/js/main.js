5d79afd88fadc5dc2a7a6df96e43e075aae94cbe
aynurajafarova <aynurafj@code.edu.az>
2020-11-20 11:46:39 +0400

    drag and drop functionality. removed unnecessary codes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ const toDoList = document.querySelector(".todo-list");
 const editForm = document.querySelector(".edit-form");
 const editInput = document.querySelector(".edit-input");
 const editToDoBtn = document.querySelector(".edit-todo-btn");
-const completeToDoList = document.querySelector(".completed-todo-list ");
+const completedToDoList = document.querySelector(".completed-todo-list ");
 
 let toDos = [];
 let completedToDos = [];
@@ -15,7 +15,6 @@ const generateRandomId = () => Math.ceil(Math.random() * 1000);
 
 const addToDo = (id) => {
   let todoText = toDoInput.value;
-  console.log(toDoInput.value);
   const newToDo = {
     id,
     todoText,
@@ -23,16 +22,18 @@ const addToDo = (id) => {
   };
   todoText.length !== 0 && toDos.push(newToDo);
   toDoInput.focus();
-
-  console.log(toDos);
 };
 
-const addToDoListItem = (id) => {
+const addToDoListItem = (id, isCompleted) => {
   const toDoText = document.createElement("li");
   const toDoTextContent = document.createElement("span");
   toDoTextContent.textContent = toDoInput.value;
   toDoText.setAttribute("id", `${id}`);
-  toDoText.setAttribute("draggable", true);
+  toDoText.setAttribute("draggable", "true");
+  toDoText.addEventListener("dragstart", (e) =>
+    e.dataTransfer.setData("item", e.target.id)
+  );
+  toDoText.addEventListener("dragover", (e) => e.preventDefault());
 
   // complete todo button
   const completedBtn = document.createElement("button");
@@ -58,90 +59,61 @@ const addToDoListItem = (id) => {
   // edit todo button
   const editBtn = document.createElement("button");
   editBtn.className = "edit-todo-btn icon-pen-solid";
-  
+
   // opens edit form
   editBtn.addEventListener("click", (e) => {
     editToDoItem(id, toDoTextContent.textContent);
     updatedToDoId = e.target.parentNode.id;
-    console.log("updatedToDoId + "+updatedToDoId)
   });
-  // toDoText.addEventListener("ondragstart", onDragStart)
   toDoText.appendChild(toDoTextContent);
   toDoText.appendChild(editBtn);
   toDoText.appendChild(completedBtn);
   toDoText.appendChild(deleteBtn);
 
   toDoInput.value.length > 0 && toDoList.appendChild(toDoText);
-  // completeToDoList.addEventListener("ondragover", onDragOver);
-  // completeToDoList.addEventListener("ondrop", onDrop);
-};
+}; 
 
 const submitForm = (e) => {
   e.preventDefault();
   let id = generateRandomId();
   addToDo(id);
-  addToDoListItem(id);
+  addToDoListItem(id, false);
   console.log(toDos);
   toDoInput.value = ""; // clear input value
 };
 
+addToDoBtn.addEventListener("click", submitForm);
+
 // remove from toDos array
 const deleteToDo = (id) => {
-  console.log("delete + " + id);
-  //    toDos.filter((item) =>{return item !== id});
-  // toDos.splice(id, 1);
   toDos = toDos.filter((item, index) => item.id != id);
-  console.log("todos + ", toDos);
 };
 
-function deleteFromToDoList(item) {
-  console.log(item);
+const deleteFromToDoList = (item) => {
   item.remove();
-}
+};
 
 // remove todo
 const deleteToDoItem = (e) => {
   let id = e.target.parentNode.id;
-  console.log("e.target.parentNode.id + " + e.target.parentNode.id);
   deleteToDo(id);
-  // let todoListItemDiv = document.querySelector("li");
-  // todoListItemDiv.parentNode.removeChild(todoListItemDiv);
   deleteFromToDoList(e.target.parentNode);
-  console.log("deleteToDoItem + ", toDos);
 };
 
-addToDoBtn.addEventListener("click", submitForm);
-
-const completeToDo = (id) => {
-  for (let i = 0; i < toDos.length; i++) {
-    let element = toDos[i];
-    if (element.id == id) {
-      toDos[i].isCompleted = !toDos[i].isCompleted;
-      // document.querySelector("li").id.className="completed"
-      console.log(element);
-      addClass(id);
-    }
-  }
-  // addClass(id)
-};
-
-const addClass = (id) => {
-  const todoList = document.querySelectorAll("li");
-  for (let i = 0; i < todoList.length; i++) {
-    const element = todoList[i];
-    // console.log(element.id);
-    if (element.id == id) {
-      element.classList.add("completed");
-    }
-  }
-};
-
+// complete todo
 const completeToDoItem = (e) => {
   let id = e.target.parentNode.parentNode.id;
-  // const element = e.target.parentNode;
   completeToDo(id);
-  // console.log("id+ " + toDoList.childNodes);
-  // console.log("element+ " + element)
+};
+
+const completeToDo = (id) => {
+  toDos.forEach((todo) => {
+    if (todo.id == id) todo.isCompleted = !todo.isCompleted;
+    console.log(todo);
+    todo.isCompleted
+      ? (document.getElementById(`${todo.id}`).className = "completed")
+      : (document.getElementById(`${todo.id}`).className = "");
+  });
 };
 
 // edit todo
@@ -159,80 +131,29 @@ const editToDoItem = (id, text) => {
   editInput.value = text;
 };
 
-const updatedToDo = (id) => {
-  console.log("updatedToDo + " + id);
+//drag drop
+const dropToList = (e, list) => {
+  const id = parseInt(e.dataTransfer.getData("item"));
+
+  toDos.forEach((todo) => {
+    if (todo.id === id) {
+      if (list == toDoList) {
+        todo.isCompleted = false;
+        document.getElementById(`${todo.id}`).className = "";
+      } else if (list == completedToDoList) {
+        todo.isCompleted = true;
+        document.getElementById(`${todo.id}`).className = "completed";
+      }
+    }
+  });
+  list.appendChild(document.getElementById(`${id}`));
 };
 
-//drag drop
-
-// const todoList = document.querySelectorAll("li");
-
-// for (var i = 0; i < todoList.length; i++) {
-//   todoList[i].addEventListener('dragstart', function(e) {
-//       e.dataTransfer.effectAllowed = 'move';
-//       e.dataTransfer.setData('text', this.innerHTML);
-//       elementDragged = this;
-//   });
-//   todoList[i].addEventListener('dragend', function(e) {
-//       elementDragged = null;
-//   });
-// };
-
-// let dropTodosDiv = document.querySelector(".completed-todo-list");
-// dropTodosDiv.addEventListener('dragover', function(e) {
-//   if (e.preventDefault) {
-//       e.preventDefault();
-//   }
-//   e.dataTransfer.dropEffect = 'move';
-//   return false;
-// });
-
-// dropTodosDiv.addEventListener('dragleave', function(e) {
-//   this.className = "";
-// });
-// data transfere id'ni vermek
-
-// dropTodosDiv.addEventListener('drop', function(e) {
-//   if (e.preventDefault) e.preventDefault();
-//   if (e.stopPropagation) e.stopPropagation();
-//   // complete(1);
-//   // this.innerHTML += "" + e.dataTransfer.getData('text');
-//   // document.querySelector('#drag-elements').removeChild(elementDragged);
-//   // elementDragged = null;
-//   // var data = ev.dataTransfer.getData("text");
-//   // thisdiv = ev.target;
-//   // document.getElementById(data).insertBefore(thisdiv);
-//   // return false;
-
-// });
-
-///////////////
-// const onDragStart=(e)=>{
-// e.dataTransfer.setData("text/plain",e.target.id);
-// e.currentTarget.style.backgroundColor="yellow";
-// console.log("test")
-// }
-
-// const onDragOver=(e)=>{
-//   e.preventDefault();
-// }
-
-// const onDrop=(e)=>{
-// const id =e.dataTransfer.getData('text');
-// const draggableElement = document.getElementById(id);
-// const dropzone = e.target;
-// dropzone.appendChild(draggableElement);
-// e.target.clearData();
-// }
-
-// const todoList = document.querySelectorAll("li");
-// for (var i = 0; i < todoList.length; i++) {
-//   todoList[i].addEventListener('dragstart', function(e) {
-//       e.dataTransfer.effectAllowed = 'move';
-//       e.dataTransfer.setData('text', this.innerHTML);
-//       elementDragged = this;
-//   });
-//   todoList[i].addEventListener('dragend', function(e) {
-//       elementDragged = null;
-//   });
-// };
+completedToDoList.addEventListener("dragover", (e) => e.preventDefault());
+toDoList.addEventListener("dragover", (e) => e.preventDefault());
+//drag to right side
+completedToDoList.addEventListener("drop", (e) =>
+  dropToList(e, completedToDoList)
+);
+//drag to left side
+toDoList.addEventListener("drop", (e) => dropToList(e, toDoList));
